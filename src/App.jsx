1ff20c3f3feb7a858174d763c3c48ecd39f0fd8d
Levignoble <sakshamsinghrawat6@gmail.com>
2024-06-10 13:21:58 +0530

    created header componenet

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/sign-in' element={<Signin/>}></Route>
